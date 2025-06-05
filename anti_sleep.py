df04f7e0e96d23a09b8bbb96592d4096a1f422a3
ZgsNat <nguyenthaithinh9876@gmail.com>
2025-06-06 00:30:06 +0700

    better code

--- anti_sleep.py
+++ anti_sleep.py
@@ -1,15 +1,25 @@
 import pyautogui
 import time
+import os
+
+def sleep_computer():
+    # This command puts Windows to sleep
+    os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
 
 print("Starting prevent sleep...")
 
 try:
-    while True:
+    duration_hours = float(input("Enter duration in hours: "))
+    duration_seconds = duration_hours * 3600
+    duration_seconds = 10  # For testing
+    print(f"Preventing sleep for {duration_hours} hours...")
+    start_time = time.time()
 
-        x,y = pyautogui.position()  # Get the current mouse position
+    while time.time() - start_time < duration_seconds:
+        x, y = pyautogui.position()  # Get the current mouse position
         pyautogui.moveTo(x + 5, y + 5, duration=0.1)  # Move the mouse slightly
         pyautogui.moveTo(x - 5, y - 5, duration=0.1)
-        time.sleep(30)
+        # time.sleep(10)
 except KeyboardInterrupt:
     print("Stopping prevent sleep...")
     pass
@@ -18,7 +28,6 @@ except Exception as e:
 finally:
     print("Exiting program.")
     pyautogui.moveTo(0, 0)
-    # Move the mouse to the top-left corner on exit
-
     time.sleep(1)  # Wait a moment before exiting
+    sleep_computer()  # Put the computer to sleep after duration
     exit(0)
